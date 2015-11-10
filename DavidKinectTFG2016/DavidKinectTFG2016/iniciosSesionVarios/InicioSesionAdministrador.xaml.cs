c3c26b7e80b260097ffff43b141199ed64ac92c3
David López Santamaria <dls0010@alu.ubu.es>
2015-11-10 13:40:32 +0100

    - Modificacion InicioSesionAdministrador - Creacion EditarPacientes - Creacion EditarTerapeutas

--- DavidKinectTFG2016/DavidKinectTFG2016/iniciosSesionVarios/InicioSesionAdministrador.xaml.cs
+++ DavidKinectTFG2016/DavidKinectTFG2016/iniciosSesionVarios/InicioSesionAdministrador.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DavidKinectTFG2016.recursosAdministrador;
 
 namespace DavidKinectTFG2016.iniciosSesionVarios
 {
@@ -24,6 +25,28 @@ namespace DavidKinectTFG2016.iniciosSesionVarios
         {
             nombreUsuario = nombre;
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Metodo que lanza la ventana para editar pacientes de la BD.
+        /// </summary>
+        /// <param name="sender"></param> Boton modificar pacientes.
+        /// <param name="e"></param> Evento del boton.
+        private void buttonModificarPacientes_Click(object sender, RoutedEventArgs e)
+        {
+            EditarPacientes editarPacientes = new EditarPacientes();
+            editarPacientes.Show();
+        }
+
+        /// <summary>
+        /// Metodo que lanza la ventana para editar terapeutas de la BD.
+        /// </summary>
+        /// <param name="sender"></param> Boton modificar terapeutas.
+        /// <param name="e"></param> Evento del boton.
+        private void buttonModificarTerapeutas_Click(object sender, RoutedEventArgs e)
+        {
+            EditarTerapeutas editarTerapeutas = new EditarTerapeutas();
+            editarTerapeutas.Show();
         }
     }
 }
---
+++ DavidKinectTFG2016/DavidKinectTFG2016/recursosAdministrador/EditarPacientes.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DavidKinectTFG2016.recursosAdministrador
+{
+    /// <summary>
+    /// Lógica de interacción para EditarPacientes.xaml
+    /// </summary>
+    public partial class EditarPacientes : Window
+    {
+        SqlConnection conexion;
+        SqlDataAdapter adaptador;
+        DataTable dt;
+        public EditarPacientes()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Metodo que carga la tabla al abrirse la ventana.
+        /// </summary>
+        /// <param name="sender"></param> Ventana.
+        /// <param name="e"></param>Evento de ventana.
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            conexion = BDComun.ObtnerConexion();
+
+            string query = "Select * from pacientes";
+            SqlCommand comando = new SqlCommand(query, conexion);
+            comando.ExecuteNonQuery();
+
+            adaptador = new SqlDataAdapter(comando);
+            dt = new DataTable("pacientes");
+            adaptador.Fill(dt);
+            dataGrid.ItemsSource = dt.DefaultView;
+            adaptador.Update(dt);
+        }
+
+        /// <summary>
+        /// Metodo que cierra la conexion con la BD al cerrar la ventana.
+        /// </summary>
+        /// <param name="sender"></param> Cerrar ventana.
+        /// <param name="e"></param> Evento de cerrar.
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            conexion.Close();
+        }
+
+        /// <summary>
+        /// Metodo que actualiza un cambio del datagrid en la BD.
+        /// </summary>
+        /// <param name="sender"></param> Boton Actualizar.
+        /// <param name="e"></param> Evento del boton.
+        private void buttonModificar_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(adaptador);
+            adaptador.UpdateCommand = builder.GetUpdateCommand();
+            int numeroCambios = adaptador.Update(dt);
+            if (numeroCambios > 0)
+                MessageBox.Show("Actualizado");
+            else
+                MessageBox.Show("No ha sido posible actualizar");
+        }
+    }
+}
